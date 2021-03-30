14387cb405ae77fb18ed6c35589e096e6b6187a0
devbg21 <juhcosta23@gmail.com>
2021-03-30 19:04:08 -0300

    trabalhando front-end

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
+    session_start();
     require __DIR__ . '/vendor/autoload.php';
+
+    define('INCLUDE_PATH_STATIC', 'http://localhost/RedeSocial/RedeSocial/Views/src/');
 
     $app = new RedeSocial\Application();
     $app->run();
+
+    
